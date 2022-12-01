4e846abb13edf784621346e3639aa675cdaa0aa7
ihor000 <ihor.silnov@yandex.ru>
2022-12-02 00:58:21 +0300

    text

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число : ");
+int namber = Convert.ToInt32(Console.Read());
+if (namber % 2 == 0)
+{
+ Console.WriteLine("Четное");   
+}
+
+else
+{
+   Console.WriteLine("Нечетное"); 
+}
