32560cf805fd3ccbd3d72030088fa2ad01cd6131
ihor000 <ihor.silnov@yandex.ru>
2022-12-02 00:19:22 +0300

    text

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,3 +4,19 @@ int namber1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
+if (namber1 > number2)
+
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+else if (namber1 < number2)
+
+{
+    Console.WriteLine("Второе число больше первого");
+}
+
+else 
+{
+    Console.WriteLine("Числа равны");
+} 
---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] numbers = new int[3];
+
+Console.WriteLine("Введите первое число: ");
+numbers[0] = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+numbers[1] = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+numbers[2] = int.Parse(Console.ReadLine());
+
+int max = numbers[0];
+int i = 1;
+
+while (i < 3)
+{
+    if(numbers[i] > max)
+        max = numbers[i];
+    i = i + 1;   
+}
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
