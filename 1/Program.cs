a6a04678725c71c6054a552f47a8371a8d903ee9
ihor000 <ihor.silnov@yandex.ru>
2022-12-01 22:00:40 +0300

    text

---
+++ 1/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Введите первое число:");
+int namber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
